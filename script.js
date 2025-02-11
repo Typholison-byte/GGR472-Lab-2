4a42cbbf1ed98b8731da50bd0882d03d7b991be2
Typholison-byte <ericd.lam@mail.utoronto.ca>
2025-02-11 14:30:00 -0500

    Refine #1
    Added more code to refine the website

--- script.js
+++ script.js
@@ -1,38 +1,3 @@
-/*mapboxgl.accessToken = 'pk.eyJ1IjoibGFrZWVyaWUiLCJhIjoiY201cG5nbmptMDM0eDJxb215YXB0OGV0ZSJ9.yGNd3OQ2HqXdTSTuJcD9ug'; // Add default public map token from your Mapbox account 
-
-const map = new mapboxgl.Map({
-    container: 'my-map', // map container ID 
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL 
-    center: [-80, 50], // starting position [lng, lat] 
-    zoom: 4, // starting zoom level
-});
-
-map.on('load', () => {
-    // Add a data source containing GeoJSON data     
-    map.addSource('uoft-data', {
-        type: 'geojson',
-        data: {
-            "type": "FeatureCollection", 
-            "features": [{ 
-                "type": "Feature", 
-                "properties": { 
-                    "name": "Sidney Smith Hall" 
-                }, 
-                "geometry": { 
-                    "coordinates": [-79.39865237301687, 43.662343395037766], 
-                    "type": "Point" } }]
-        }
-    }); map.addLayer({ 
-        'id': 'uoft-pnt', 
-        'type': 'circle', 
-        'source': 'uoft-data', 
-        'paint': { 
-            'circle-radius': 6, 'circle-color': '#B42222' 
-        } 
-    });
-});
-*/
-
 document.addEventListener('DOMContentLoaded', () => {
     // Get references to buttons and vote count spans
     const yesButton = document.getElementById('btn-yes');
